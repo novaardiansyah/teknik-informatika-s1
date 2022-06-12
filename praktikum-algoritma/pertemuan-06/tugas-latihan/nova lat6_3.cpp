f9ea8075e1972d9cbaf7eeec7a8ebb558d5ff392
novaardiansyah <novaardiansyah78@gmail.com>
2022-06-12 15:35:18 +0700

    Complete the practicum task of module six

--- praktikum-algoritma/pertemuan-06/tugas-latihan/nova lat6_3.cpp
+++ praktikum-algoritma/pertemuan-06/tugas-latihan/nova lat6_3.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
@@ -11,4 +10,36 @@ int main()
   cout << "NIM \t: 211011401309\n";
   cout << "Kelas \t: 02-TPLE011\n";
   cout << "====================\n\n";
+
+  int nomer, bagi, sisa;
+  string pilihan;
+
+  cout << "Masukkan nomer: ";
+  cin >> nomer;
+
+  cout << "Masukkan bilangan pembagi: ";
+  cin >> bagi;
+
+  sisa = nomer % bagi;
+
+  cout << "============================\n\n";
+  cout << "Bilangan yang dipilih adalah : " << nomer << endl;
+  cout << "Bilangan pembagi adalah : " << bagi << "\n\n";
+
+  if (sisa == 0)
+  {
+    cout << "Sisa bagi tidak ada\n";
+  } else {
+    cout << "Sisa bagi adalah : " << sisa << endl;
+  }
+
+  cout << "\nApakah anda ingin mengulangi? (y/n) : ";
+  cin >> pilihan;
+
+  if (pilihan == "y")
+  {
+    main();
+  } else {
+    cout << "Terima kasih telah menggunakan program ini\n";
+  }
 }
