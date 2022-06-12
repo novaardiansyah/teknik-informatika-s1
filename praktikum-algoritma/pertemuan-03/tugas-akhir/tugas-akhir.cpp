3dd9e956af885861cfcd2548f2c9ca015951057d
novaardiansyah <novaardiansyah78@gmail.com>
2022-06-12 12:30:40 +0700

    Setting up the exercise structure for the next meeting

---
+++ praktikum-algoritma/pertemuan-03/tugas-akhir/tugas-akhir.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "\nPertemuan-03: Tugas Akhir\n";
+  cout << "=========================\n\n";
+
+  cout << "Nama \t: Nova Ardiansyah\n";
+  cout << "NIM \t: 211011401309\n";
+  cout << "Kelas \t: 02-TPLE011\n";
+  cout << "====================\n\n";
+}
