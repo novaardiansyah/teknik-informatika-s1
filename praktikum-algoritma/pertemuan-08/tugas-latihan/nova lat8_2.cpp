3a33283361e058cf5d98b7092aa49e518de08da0
novaardiansyah <novaardiansyah78@gmail.com>
2022-06-18 19:33:14 +0700

    Complete tasks at meeting eight

---
+++ praktikum-algoritma/pertemuan-08/tugas-latihan/nova lat8_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "\nPertemuan-08: Tugas Latihan 02\n";
+  cout << "==============================\n\n";
+
+  cout << "Nama \t: Nova Ardiansyah\n";
+  cout << "NIM \t: 211011401309\n";
+  cout << "Kelas \t: 02-TPLE011\n";
+  cout << "====================\n\n";
+
+  int square[100];
+  int i, k;
+
+  for (i = 0; i < 10; i++)
+  {
+    k = i + 1;
+    square[i = k * k];
+  }
+
+  printf("Pangkat dari %d adalah %d", k, square[i]);
+  return 0;
+}
