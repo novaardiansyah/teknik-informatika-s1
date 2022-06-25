fcab79a49e90133a9b9c9f02cacc72a20f10ea98
novaardiansyah <novaardiansyah78@gmail.com>
2022-06-25 19:52:56 +0700

    Complete basic tasks at the sixth meeting

---
+++ praktikum-algoritma/pertemuan-06/tugas-pendahuluan/04. tugas-pendahuluan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "\nPertemuan-06: Tugas Pendahuluan 04\n";
+  cout << "==================================\n\n";
+
+  cout << "Nama \t: Nova Ardiansyah\n";
+  cout << "NIM \t: 211011401309\n";
+  cout << "Kelas \t: 02-TPLE011\n";
+  cout << "====================\n\n";
+
+  int bulan - 10;
+
+  while (bulan < 12)
+  {
+    cout << "Bulan ke-" << bulan << endl;
+    bulan++;
+  }
+
+  return 0;
+}
